0b15e2d51f6f6ac16b276bfc3c552f9a96736749
ayush-142005 <143897438+ayush-142005@users.noreply.github.com>
2025-06-11 08:40:03 +0530

    Add files via upload

---
+++ Leetcode1431.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Leetcode1431 {
+    public int[] createTargetArray(int[] nums, int[] index) {
+      ArrayList<Integer> l1 = new ArrayList<>();
+      for (int i = 0; i < nums.length; i++) {
+        l1.add(index[i],nums[i]);
+      }
+      int result [] = new int[nums.length]; 
+      for (int i = 0; i < result.length; i++) {
+        result[i]=l1.get(i);
+      }
+      return result;
+    }
+    }
+
---
+++ palindromeStr.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class palindromeStr {
+    public static void main(String[] args) {
+        String Statement = "abcba";
+        System.out.println(palindrome(Statement));
+    }
+    static boolean palindrome(String Statement){
+        StringBuilder original = new StringBuilder(Statement);
+        StringBuilder reversed = new StringBuilder(Statement).reverse();
+        for (int i = 0; i < original.length(); i++) {
+            if (original.charAt(i)!=reversed.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
