22ccba3f667bfb395f3c530974465043d9cc9819
Ayush Hood <143897438+ayush-142005@users.noreply.github.com>
2025-07-21 06:24:33 +0530

    Add files via upload

---
+++ occurance_string.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+//public class occurance_string {
+//    public static void main(String[] args) {
+//        String name = "AABBCCDD";
+//    }
+//    static String occurance_(String name){
+//        char list1 [] = name.toCharArray();
+//        char list2 [] = new char[list1.length-1];
+//        for (int i = 0; i < list1.length; i++) {
+//            int count = 0;
+//
+//            for (int j = i; j < list1.length; j++) {
+//                if (i!=j ){
+//                    count++;
+//                }
+//            }
+//            String occurance = list1[i] +":" + count;
+//        }
+//
+//    }
+//}
